f5401ffedac21a3b84ccb8ec56fb9f9a1318eb1a
Roman <mzfkr97@gmail.com>
2020-03-17 14:49:17 +0300

    add Dagger2as

--- app/src/main/java/com/roman/batsu/utils/TextUtils.java
+++ app/src/main/java/com/roman/batsu/utils/TextUtils.java
@@ -7,7 +7,6 @@ import android.text.Layout;
 import android.text.Spannable;
 import android.text.style.ClickableSpan;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
 
 public class TextUtils {
@@ -21,39 +20,36 @@ public class TextUtils {
     @SuppressLint("ClickableViewAccessibility")
     public static void setTextWithLinks(TextView textView, CharSequence html) {
         textView.setText(html);
-        textView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                int action = event.getAction();
-                if (action == MotionEvent.ACTION_UP ||
-                        action == MotionEvent.ACTION_DOWN) {
-                    int x = (int) event.getX();
-                    int y = (int) event.getY();
+        textView.setOnTouchListener((v, event) -> {
+            int action = event.getAction();
+            if (action == MotionEvent.ACTION_UP ||
+                    action == MotionEvent.ACTION_DOWN) {
+                int x = (int) event.getX();
+                int y = (int) event.getY();
 
-                    TextView widget = (TextView) v;
-                    x -= widget.getTotalPaddingLeft();
-                    y -= widget.getTotalPaddingTop();
+                TextView widget = (TextView) v;
+                x -= widget.getTotalPaddingLeft();
+                y -= widget.getTotalPaddingTop();
 
-                    x += widget.getScrollX();
-                    y += widget.getScrollY();
+                x += widget.getScrollX();
+                y += widget.getScrollY();
 
-                    Layout layout = widget.getLayout();
-                    int line = layout.getLineForVertical(y);
-                    int off = layout.getOffsetForHorizontal(line, x);
+                Layout layout = widget.getLayout();
+                int line = layout.getLineForVertical(y);
+                int off = layout.getOffsetForHorizontal(line, x);
 
-                    ClickableSpan[] link = Spannable.Factory.getInstance()
-                            .newSpannable(widget.getText())
-                            .getSpans(off, off, ClickableSpan.class);
+                ClickableSpan[] link = Spannable.Factory.getInstance()
+                        .newSpannable(widget.getText())
+                        .getSpans(off, off, ClickableSpan.class);
 
-                    if (link.length != 0) {
-                        if (action == MotionEvent.ACTION_UP) {
-                            link[0].onClick(widget);
-                        }
-                        return true;
+                if (link.length != 0) {
+                    if (action == MotionEvent.ACTION_UP) {
+                        link[0].onClick(widget);
                     }
+                    return true;
                 }
-                return false;
             }
+            return false;
         });
     }
 
