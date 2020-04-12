17913ee17dee8632ba5debce3484fbfc36bb721d
Roman <mzfkr97@gmail.com>
2020-04-12 14:11:32 +0300

    remove Dagger2 add^ Kotlin DataBinding Lottie Coroutines

--- app/src/main/java/com/roman/batsu/ui/rings/RingsFragment.kt
+++ app/src/main/java/com/roman/batsu/ui/rings/RingsFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.roman.batsu.R
+import com.roman.batsu.databinding.FragmentNotificationsBinding
 import com.roman.batsu.ui.model.Rings
 import com.roman.batsu.utils.JSONReader
 import java.util.*
@@ -20,6 +22,7 @@ class RingsFragment : Fragment() {
     private lateinit var adapter: RingsAdapter
     private var notificationArrayList: MutableList<Rings> = ArrayList()
     private val jsonReader = JSONReader()
+    private lateinit var notificationsBinding: FragmentNotificationsBinding
 
     companion object {
         fun newInstance(): RingsFragment {
@@ -28,22 +31,28 @@ class RingsFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_notifications, container, false)
-        setUpRecyclerView(view)
-        notificationArrayList.clear()
-        val descriptionHeader = view.findViewById<TextView>(R.id.descriptionHeader)
-        descriptionHeader.text = getString(R.string.description_header_notify)
-        val inputStream = resources.openRawResource(R.raw.ring_lessons)
-        notificationArrayList = jsonReader.createListJson(inputStream)
-        adapter = RingsAdapter(activity, notificationArrayList)
-        recyclerView.adapter = adapter
-        return view
-    }
+        notificationsBinding = DataBindingUtil.inflate(
+                inflater,
+                R.layout.fragment_notifications,
+                container,
+                false)
 
-    private fun setUpRecyclerView(view: View) {
-        recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = GridLayoutManager(activity, 2)
+        notificationsBinding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = GridLayoutManager(activity, 2)
+            notificationArrayList.clear()
+            notificationsBinding.itemFooter.descriptionHeader.text = getString(R.string.description_header_notify)
+            val inputStream = resources.openRawResource(R.raw.ring_lessons)
+            notificationArrayList = jsonReader.createListJson(inputStream)
+
+            adapter = RingsAdapter(activity, notificationArrayList)
+            notificationsBinding.recyclerView.adapter = adapter
+
+        }
+
+
+        return notificationsBinding.root
+
     }
 
 
